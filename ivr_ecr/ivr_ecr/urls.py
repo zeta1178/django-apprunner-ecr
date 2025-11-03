270df57cecb597200780ef4f71262bed37059e9f
Michael Cruz <michaelcruz@aim.com>
2025-11-02 23:43:12 -0500

    edit

--- ivr_ecr/ivr_ecr/urls.py
+++ ivr_ecr/ivr_ecr/urls.py
@@ -22,7 +22,7 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # path('', TemplateView.as_view(template_name='home.html'), name='home'),
+    path('', TemplateView.as_view(template_name='home.html'), name='home'),
     # path('', views.home, name='home'),
-    # path('api/', include('api.urls')),
+    path('api/', include('api.urls')),
 ]
